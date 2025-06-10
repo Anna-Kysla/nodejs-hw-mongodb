4eb4d7ba87a822191dd83be45194c649eb65247c
Anna K <1415ks@ukr.net>
2025-06-10 20:53:44 +0300

    thirteenth

--- src/setupServer.js
+++ src/setupServer.js
@@ -1,14 +1,28 @@
 import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
 import contactsRouter from './routes/contactsRouter.js';
 
 export default function setupServer() {
   const app = express();
 
+  /*app.use((req, res, next) => {
+    console.log(`Incoming request: ${req.method} ${req.url}`);
+    next();
+  });*/
+
+  // 🛡 Додайте CORS
+  app.use(cors());
+
+  // 📋 Додайте логер Pino
+  app.use(pino());
+
   app.use(express.json());
 
+  // 📁 Роут для контактів
   app.use('/contacts', contactsRouter);
 
-  // Обробка невідомих маршрутів
+  // ❌ Обробка невідомих маршрутів
   app.use((req, res) => {
     res.status(404).json({ message: 'Not found' });
   });
